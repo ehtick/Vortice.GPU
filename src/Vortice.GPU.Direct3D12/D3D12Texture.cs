4baafdd8cc3889ba5647337a05835c30a6b273a9
Amer Koleci <amerkoleci@gmail.com>
2021-12-12 10:26:30 +0100

    GPU: Separate backends and stub for D3D12, target .netstandard2.0 as well.

---
+++ src/Vortice.GPU.Direct3D12/D3D12Texture.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Amer Koleci and contributors.
+// Distributed under the MIT license. See the LICENSE file in the project root for more information.
+
+using Vortice.Direct3D12;
+using static Vortice.Direct3D12.D3D12;
+
+namespace Vortice.GPU.Direct3D12;
+
+internal unsafe class D3D12Texture : Texture
+{
+    public D3D12Texture(D3D12Device device) : base(device)
+    {
+    }
+
+    public ID3D12Resource Handle { get; }
+}
